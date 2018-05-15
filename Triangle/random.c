826df68eac5856fc04bc6cdba64b31507dc47b03
Amy Wong <Amy Wong@WINTER>
2018-05-15 17:51:08 -0400

    Area of triangle

--- Triangle/random.c
+++ Triangle/random.c
@@ -1,25 +1,21 @@
-// include library
 #include <stdio.h>
-//calculate area of triangle
+
 int main(void)
 {
-	float height, width;
-	float product;
-	float area;
+	float height, width, area;
 
-	//prompt user to enter the height and base
-	// accepts user input in decimal format
+	// User input for height and width
 	printf("Enter height of triangle: ");
 	scanf("%f", &height);
 	printf("Enter width of triangle: ");
 	scanf("%f", &width);
-	//// multiply the product of the height and base by 0.5
-	product = height * width;
-	area = product * 0.5;
-	//// stores area in memory and outputs area to 2 decimal places along with its address in memory in hexadecimal format
-	//// use %p conversation specifier to output an address 
-	printf("The area of the triangle is %f\n", area);
+
+	area = (height * width) * 0.5;
+	
+	// Output for area memory and value
+	printf("The area of the triangle is %.2f\n", area);
 	printf("The address of the triangle's area is in %p", &area);
+	
 	return 0;
 	
 }
